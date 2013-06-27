660f17fc8d651a5b117468d8185788a2c3af1c6f
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-06-27 11:09:35 -0700

    Install Course Themes

---
+++ theme/intro_to_business/lib.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Functions needed by the intro_to_business theme should be put here.
+ *
+ * Any functions that get created here should ALWAYS contain the theme name
+ * to reduce complications for other theme designers who may be copying this theme.
+ */
