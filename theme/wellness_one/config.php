a72be2af052869463c58f100784b76bede2fce8c
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-07-31 15:37:24 -0700

    Adding the jquery files to Well1 theme

--- theme/wellness_one/config.php
+++ theme/wellness_one/config.php
@@ -131,5 +131,5 @@ $THEME->layouts = array(
 $THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->enable_dock = true;
 //$THEME->javascripts_footer = array('navigation');
-//$THEME->javascripts = array('skonline' => array('skonline'));  Trying to fix the absent SAVE button
+$THEME->javascripts = array('skonline' => array('skonline','jquery-1.10.1.min','jquery_expander_1.0','jquery_expander_custom'));  
 $THEME->editor_sheets = array('editor');
