2e539a0474514ff8ac0e8504446c0f99872b6634
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-07-31 13:22:16 -0700

    Revert "Trying to get jquery to load"
    This reverts commit 273ad9e52c99ea22f9337721486e264c7c58c70b.

--- theme/skonline/lib.php
+++ theme/skonline/lib.php
@@ -6,6 +6,3 @@
  * Any functions that get created here should ALWAYS contain the theme name
  * to reduce complications for other theme designers who may be copying this theme.
  */
- function theme_skonline_page_init(moodle_page $page) {
-    $page->requires->jquery();
-}
