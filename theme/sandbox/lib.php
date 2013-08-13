aeda4f0b35f2f9ec74e0fddc02eabec36ebb5a38
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-08-13 08:34:03 -0700

    theme stuff and new sandbox theme

---
+++ theme/sandbox/lib.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Functions needed by the sandbox theme should be put here.
+ *
+ * Any functions that get created here should ALWAYS contain the theme name
+ * to reduce complications for other theme designers who may be copying this theme.
+ */
+ 
+  function theme_sandbox_page_init(moodle_page $page) {
+    $page->requires->jquery();
+    $page->requires->jquery_plugin('expander', 'theme_skonline');
+	$page->requires->jquery_plugin('expander_custom', 'theme_skonline');
+ }
