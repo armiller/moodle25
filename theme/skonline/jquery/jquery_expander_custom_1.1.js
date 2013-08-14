8e16b43f460ef25e0e3cd64932a6c42b6d26f3ec
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-08-14 08:43:42 -0700

    change expand text

--- theme/skonline/jquery/jquery_expander_custom_1.1.js
+++ theme/skonline/jquery/jquery_expander_custom_1.1.js
@@ -20,8 +20,8 @@ $(document).ready(function() {
   
    $('.announcement h1:gt(0)~*').expander({
     slicePoint:       80,  // default is 100
-  
-    
+   expandText: 'more',
+    expandPrefix: '...',
 
   });
 
