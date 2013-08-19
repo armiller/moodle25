455f44995b2e2fe78f29ee21e50d0e26efeef20c
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-08-19 09:58:33 -0700

    Intro to Bus Announcement update

--- theme/intro_to_business/lib.php
+++ theme/intro_to_business/lib.php
@@ -6,3 +6,10 @@
  * Any functions that get created here should ALWAYS contain the theme name
  * to reduce complications for other theme designers who may be copying this theme.
  */
+ 
+  function theme_intro_to_business_page_init(moodle_page $page) {
+    $page->requires->jquery();
+    $page->requires->jquery_plugin('expander', 'theme_skonline');
+	$page->requires->jquery_plugin('expander_custom', 'theme_skonline');
+ }
+ ?>
