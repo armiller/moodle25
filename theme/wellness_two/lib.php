0a5fa8d7b6e3d05093b8b9ff0536cdd6a546b5f1
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-08-23 11:53:35 -0700

    Revert "remove announcement lib"
    This reverts commit 0a1f0fc80b382bf904d1a1b67a76bb90d085564d.

--- theme/wellness_two/lib.php
+++ theme/wellness_two/lib.php
@@ -7,5 +7,10 @@
  * to reduce complications for other theme designers who may be copying this theme.
  */
  
-
+ function theme_wellness_two_page_init(moodle_page $page) {
+    $page->requires->jquery();
+    $page->requires->jquery_plugin('expander', 'theme_skonline');
+	$page->requires->jquery_plugin('expander_custom', 'theme_skonline');
+ }
+ ?>
  
