6eedf0db4a234ec55c37c77f5de3524eb491bd84
Kimber Warden <warden_kimber@salkeiz.k12.or.us>
2013-09-05 15:53:15 -0700

    Comment out renderer override

--- theme/skonline/config.php
+++ theme/skonline/config.php
@@ -134,7 +134,7 @@ $THEME->layouts = array(
     ),
 );
 
-$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+//$THEME->rendererfactory = 'theme_overridden_renderer_factory';
 $THEME->enable_dock = true;
 $THEME->editor_sheets = array('editor');
 $THEME->javascripts = array('skonline', 'jquery-1.10.1.min','jquery_expander_1.0','jquery_expander_custom_1.0');
